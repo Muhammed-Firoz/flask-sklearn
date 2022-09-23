f8963ca1e09bb5d79e25cafffcb61bcb340170b3
Muhammed Firoz <firozmailhome@gmail.com>
2022-09-23 13:15:13 +0000

    Updating the make_predict_azure.sh

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://flaskmlapp208378.azurewebsites.net:$PORT/predict 
+     -X POST https://firozflaskmlapp.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
